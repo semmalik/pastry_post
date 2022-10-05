568e499d7e5b13fe9c617b6e02dd5b32349ebb1e
Sarah Malik <sarah.e.mower@gmail.com>
2022-10-04 18:56:18 -0700

    adding style to gallery, menu, message pages

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin");
 const { ensureAuth, ensureGuest, ensureAdmin } = require("../middleware/auth");
 
-router.get("/", adminController.getDashboard);
+router.get("/", ensureAuth, ensureAdmin, adminController.getDashboard);
 router.get("/getMessage/:id", ensureAuth, adminController.getMessage)
 router.post("/replyMessage/:id", ensureAuth, ensureAdmin, adminController.replyMessage)
 
